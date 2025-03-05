b0f48a20a69a5dfe8b7c4538b1b2146117c7b680
Stan Ovchinnikov <lishnih@gmail.com>
2025-03-05 23:24:49 +0300

    Code implemented

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+import WidgetExtractData from '@/components/widget-extract-data.vue'
+
+</script>
+
+<template>
+  <main>
+    <div>
+      <h3>Convert raw data to list</h3>
+      <WidgetExtractData />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    padding: 10px;
+    background-color: whitesmoke;
+  }
+  div {
+    padding: 10px;
+    background-color: white;
+  }
+</style>
